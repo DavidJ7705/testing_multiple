fa7a50ea731f4d9e761e9e1295c136a4f6b9c2a8
G00419108@atu.ie <G00419108@atu.ie>
2024-12-10 12:52:23 +0000

    adding all files

---
+++ src/main/java/ie/atu/processpayment/PaymentController.java
@@ -0,0 +1,15 @@
+package ie.atu.processpayment;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    @PostMapping
+    public String makePayment(@RequestBody Person person) {
+        return "Payment microservice";
+    }
+}
